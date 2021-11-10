bcd0a429f4e9844cb9fcc47de3289bc3e4769dea
清露晨流 <74122331+grey-wings@users.noreply.github.com>
2021-11-10 21:48:13 +0800

    Add files via upload

---
+++ pdfOperator/Version_2021.11.10/qtDesigner files/transferMainWindow.bat
@@ -0,0 +1,3 @@
+E:
+cd E:\SME\Data_Science\PDFOperator_210706\venv\Scripts
+pyuic5 -o MainWindow.py MainWindow.ui
---
+++ pdfOperator/Version_2021.11.10/qtDesigner files/transferPictures.bat
@@ -0,0 +1,3 @@
+E:
+cd E:\SME\Data_Science\PDFOperator_210706\venv\Scripts
+pyrcc5 pictures.qrc -o pictures_rc.py
